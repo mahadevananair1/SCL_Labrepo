31816eb24af7438bd71e4e8711cc0fc7c57c5746
Mahadevan A Nair <66215942+mahadevananair1@users.noreply.github.com>
2020-10-28 11:22:55 +0530

    Create lab6.3.py

---
+++ lab6.3.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as pl
+import numpy as np
+
+x = np.linspace(-np.pi, np.pi, 256, endpoint=True)
+
+y = np.cos(x)
+y1 = np.sin(x)
+
+#cos plot
+pl.plot(x,y)
+# sin plot
+pl.plot(x, y1)
+pl.show()
